2ee4df52e72aa642ce9995c4947e7c7fd0957032
kevin-navarro <kevin.olayon@live.com>
2023-10-26 10:58:51 -0300

    Equipments animations with player movement

--- Dungeon Merchant/Assets/Development/_Scripts/Player.cs
+++ Dungeon Merchant/Assets/Development/_Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,8 +8,10 @@ public class Player : MonoBehaviour
 
     [SerializeField] Rigidbody2D rb;
     [SerializeField] Animator anim;
+    [SerializeField] List<Animator> equipmentAnimList;
 
     IInteractable currentInteractable;
+    Vector2 movement;
 
     private void Update()
     {
@@ -27,7 +30,7 @@ public class Player : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         // Movement vector calculation
-        Vector2 movement = new Vector2(horizontalInput, verticalInput).normalized * moveSpeed;
+        movement = new Vector2(horizontalInput, verticalInput).normalized * moveSpeed;
 
         // Apply the movement to the Rigidbody2D
         rb.velocity = movement;
@@ -40,6 +43,28 @@ public class Player : MonoBehaviour
         }
 
         anim.SetFloat("speed", movement.sqrMagnitude);
+
+        EquipmentAnim();
+    }
+
+    public void EquipmentAnim()
+    {
+        if (equipmentAnimList.Count <= 0) return;
+
+        foreach (Animator anim in equipmentAnimList)
+        {
+            if (anim.gameObject.activeInHierarchy)
+            {
+                // Set animation parameters
+                if (movement.sqrMagnitude > 0)
+                {
+                    anim.SetFloat("horizontal", movement.x);
+                    anim.SetFloat("vertical", movement.y);
+                }
+
+                anim.SetFloat("speed", movement.sqrMagnitude);
+            }
+        }
     }
 
     public void InteractInput()
@@ -55,11 +80,16 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Check if already has a interface reference
         if (currentInteractable != null) return;
 
+        // Try get interactable interface
         if (collision.TryGetComponent(out IInteractable interactable))
         {
+            // Show interact text
             CanvasManager.Instance.ToggleInteractText(true);
+
+            // Get interface reference
             currentInteractable = interactable;
         }
     }
@@ -68,7 +98,10 @@ public class Player : MonoBehaviour
     {
         if (collision.TryGetComponent(out IInteractable interactable))
         {
+            // Show interact text
             CanvasManager.Instance.ToggleInteractText(false);
+
+            // Remove interface reference
             currentInteractable = null;
         }
     }
