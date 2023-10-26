e79bc0076fceefed13842f17ed870d8a73057c6e
kevin-navarro <kevin.olayon@live.com>
2023-10-25 21:37:16 -0300

    Canvas, player movement, shop and gear/inventorty setup

---
+++ Dungeon Merchant/Assets/Development/_Scripts/ItemListSO.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptables", fileName = "ItemList")]
+public class ItemListSO : ScriptableObject
+{
+    public List<Item> itemList;
+}
+
+[System.Serializable]
+public class Item
+{
+    public string itemName;
+    public Sprite itemIcon;
+    public int buyPrice;
+    public int sellPrice;
+}
---
+++ Dungeon Merchant/Assets/Development/_Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed; // Control the movement speed
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        // Input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Movement vector calculation
+        Vector2 movement = new Vector2(horizontalInput, verticalInput).normalized * moveSpeed;
+
+        // Apply the movement to the Rigidbody2D
+        rb.velocity = movement;
+    }
+}
